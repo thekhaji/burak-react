1ddccc671016bd5ce7992e12afe68b854877ca59
Khojiakbar <khojiakbar_kodirov@mail.ru>
2024-07-09 00:55:46 +0900

    fix: develop burak screen based components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,8 +5,12 @@ import '../css/app.css';
 import { Box, Button, Container, Stack, Typography } from '@mui/material';
 
 import { Link, Route, Switch } from 'react-router-dom';
-import { About } from './screens/About';
-import { Users } from './screens/Users';
+
+import { HomePage } from './screens/homePage';
+import { ProductsPage } from './screens/productsPage';
+import { OrdersPage } from './screens/ordersPage';
+import { UserPage } from './screens/userPage';
+import { HelpPage } from './screens/helpPage';
 
 function App() {
   return (
@@ -17,10 +21,16 @@ function App() {
           <Link to="/">Home</Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <Link to="/products">ProductsPage</Link>
         </li>
         <li>
-          <Link to="/users">Users</Link>
+          <Link to="/orders">OrdersPage</Link>
+        </li>
+        <li>
+          <Link to="/member-page">UserPage</Link>
+        </li>
+        <li>
+          <Link to="/help">HelpPage</Link>
         </li>
       </ul>
     </nav>
@@ -28,22 +38,26 @@ function App() {
     {/* A <Switch> looks through its children <Route>s and
         renders the first one that matches the current URL. */}
     <Switch>
-      <Route path="/about">
-        <About />
+      <Route path="/products">
+        <ProductsPage />
       </Route>
-      <Route path="/users">
-        <Users />
+      <Route path="/orders">
+        <OrdersPage />
+      </Route>
+      <Route path="/member-page">
+        <UserPage />
+      </Route>
+      <Route path="/help">
+        <HelpPage />
       </Route>
       <Route path="/">
-        <Home />
+        <HomePage />
       </Route>
     </Switch>
   </div>);
 }
 
-function Home() {
-  return <h2>Home</h2>;
-}
+
 
 
 
