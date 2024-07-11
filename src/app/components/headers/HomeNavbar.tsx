cd0129c40a6ac6973b19729fb1cc95953b5d26cd
Khojiakbar <khojiakbar_kodirov@mail.ru>
2024-07-11 20:41:59 +0900

    fix: homeNavbar menu part refactoring

--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -4,14 +4,14 @@ import { NavLink } from "react-router-dom";
 export function HomeNavbar(){
     const authMemeber = null;
     return <div className="home-navbar">
-        <Container sx={{mt:"55px", height: "642px"}}>
-            <Stack sx={{height: "50px"}} flexDirection={"row"} justifyContent={"space-between"} alignItems={"center"}>
+        <Container className="navbar-container">
+            <Stack className="menu">
                 <Box>
                     <NavLink to={"/"}>
-                        <img src="/icons/burak.svg" alt="" style={{width: "125px", height: "30px"}}/>
+                        <img className="brand-logo" src="/icons/burak.svg"/>
                     </NavLink>
                 </Box>
-                <Stack flexDirection={"row"} justifyContent={"space-between"} minWidth={"700px"} alignItems={"center"}>
+                <Stack className="links">
                     <Box className = {"hover-line"}>
                         <NavLink to={"/"} activeClassName={"underline"}>Home</NavLink>
                     </Box>
@@ -29,7 +29,9 @@ export function HomeNavbar(){
                     <Box className = {"hover-line"}>
                         <NavLink to={"/help"} activeClassName={"underline"}>Help</NavLink>
                     </Box>
-                    {!authMemeber ? (<Box><Button variant="contained" sx={{background: "#3776CC", color: "#f8f8ff"}}>Login</Button></Box>) : (<img/>)}
+                    {!authMemeber ? 
+                        (<Box><Button variant="contained" className="login-button">Login</Button></Box>) : 
+                        (<img className="user-avatar" src={"/icons/default-user.svg"} aria-haspopup={"true"} />)}
                 </Stack>
             </Stack>
             <Stack>DETAIL</Stack>
