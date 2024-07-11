8ace91c437f9965964cf48814cc26d8d581e0cde
Khojiakbar <khojiakbar_kodirov@mail.ru>
2024-07-11 20:23:54 +0900

    fix: modify root index render logic

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './app/App';
@@ -10,8 +10,10 @@ import theme from './app/MaterialTheme';
 import './css/index.css';
 import { BrowserRouter as Router} from "react-router-dom";
 
+const container = document.getElementById("root")!;
+const root = createRoot(container)
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
@@ -21,8 +23,7 @@ ReactDOM.render(
         </Router> 
       </ThemeProvider>
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
