ae0256a87ba1bc1dcbd9a61a324485e06cd1d270
Khojiakbar <khojiakbar_kodirov@mail.ru>
2024-07-13 21:03:02 +0900

    feat: organize homepage related sectional components

---
+++ src/app/screens/homePage/Advertisement.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container } from "@mui/material";
+
+export default function Advertisement(){
+    return (
+        <div>
+           Advertisement
+        </div>
+    );
+}
