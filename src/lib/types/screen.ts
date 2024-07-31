20461cb06eaba59357e7f55f5c0f3d7c734a8d50
Khojiakbar <khojiakbar_kodirov@mail.ru>
2024-07-31 20:51:18 +0900

    feat: develop AppRootState and HomePage screen component type integration

---
+++ src/lib/types/screen.ts
@@ -0,0 +1,19 @@
+import { Member } from "./member";
+import { Product } from "./product";
+
+
+/** REACT APP STATE **/
+export interface AppRootState{
+    homePage: HomePageState;
+}
+
+/** HOMEPAGE **/
+export interface HomePageState{
+    popularDishes: Product[];
+    newDishes: Product[];
+    topUsers: Member[];
+}
+
+/** PRODUCTS PAGE **/
+
+/** ORDERS PAGE **/
