2b3af5a6052b65484d2ff4d1ffc9ce3bdcc0102d
Khojiakbar <khojiakbar_kodirov@mail.ru>
2024-08-08 13:44:35 +0900

    fix: modify chosenProduct component logic

--- src/app/screens/productsPage/ChosenProduct.tsx
+++ src/app/screens/productsPage/ChosenProduct.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, Stack, Box } from "@mui/material";
 import { Swiper, SwiperSlide } from "swiper/react";
 import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
@@ -20,6 +20,10 @@ import { useSelector } from "react-redux";
 import { createSelector } from "reselect";
 import { retrieveChosenProduct, retrieveRestaurant } from "./selector";
 import { Member } from "../../../lib/types/member";
+import { useParams } from "react-router-dom";
+import ProductService from "../../services/ProductService";
+import MemberService from "../../services/MemberService";
+import { serverApi } from "../../../lib/config";
 
 /* REDUX SLICE & SELECTOR */
 const actionDispatch = (dispatch: Dispatch) => ({
@@ -39,6 +43,30 @@ const chosenProductRetriever = createSelector(
 
 
 export default function ChosenProduct() {
+  const {setRestaurant, setChosenProduct} = actionDispatch(useDispatch());
+  const {productId} = useParams<{productId: string}>();
+  const {chosenProduct} = useSelector(chosenProductRetriever);
+  const {restaurant} = useSelector(restaurantRetriever);
+
+  console.log("productId:", productId);
+  
+  useEffect(()=>{
+    const product = new ProductService();
+    product.getProduct(productId)
+      .then((data)=>setChosenProduct(data))
+      .catch((err)=>console.log(err)
+      )
+
+    const member = new MemberService();
+    member.getRestaurant()
+      .then((data)=> setRestaurant(data))
+      .catch((err)=>console.log(err))
+
+  },[]);
+
+  if (!chosenProduct)
+    return null;
+
   return (
     <div className={"chosen-product"}>
       <Box className={"title"}>Product Detail</Box>
@@ -51,11 +79,12 @@ export default function ChosenProduct() {
             modules={[FreeMode, Navigation, Thumbs]}
             className="swiper-area"
           >
-            {["/img/cutlet.webp", "/img/kebab-fresh.webp"].map(
+            {chosenProduct?.productImages.map(
               (ele: string, index: number) => {
+                const imagePath = `${serverApi}/${ele}`;
                 return (
                   <SwiperSlide key={index}>
-                    <img className="slider-image" src={ele} />
+                    <img className="slider-image" src={imagePath} />
                   </SwiperSlide>
                 );
               }
@@ -64,22 +93,23 @@ export default function ChosenProduct() {
         </Stack>
         <Stack className={"chosen-product-info"}>
           <Box className={"info-box"}>
-            <strong className={"product-name"}>Kebab</strong>
-            <span className={"resto-name"}>Burak</span>
+            <strong className={"product-name"}>{chosenProduct.productName}</strong>
+            <span className={"resto-name"}>{restaurant?.memberNick}</span>
+            <span className={"resto-name"}>{restaurant?.memberPhone}</span>
             <Box className={"rating-box"}>
               <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
               <div className={"evaluation-box"}>
                 <div className={"product-view"}>
                   <RemoveRedEyeIcon sx={{ mr: "10px" }} />
-                  <span>20</span>
+                  <span>{chosenProduct?.productViews}</span>
                 </div>
               </div>
             </Box>
-            <p className={"product-desc"}>Our best product</p>
+            <p className={"product-desc"}>{chosenProduct?.productDesc ? chosenProduct?.productDesc : "No DESCRIPTION!"}</p>
             <Divider height="1" width="100%" bg="#000000" />
             <div className={"product-price"}>
               <span>Price:</span>
-              <span>$12</span>
+              <span>${chosenProduct.productPrice}</span>
             </div>
             <div className={"button-box"}>
               <Button variant="contained">Add To Basket</Button>
